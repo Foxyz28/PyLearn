519cca623a289c0f412b4113e74d22050668bb89
Yuanzhe <h568018728@gmail.com>
2018-10-18 17:29:23 +0800

    Default

--- 001_test.py
+++ 001_test.py
@@ -1,2 +1,13 @@
-n1 = 16
-print(hex(n1))
+#coding:utf-8
+
+from urllib import request
+
+page = request.urlopen('https://www.digikey.com.cn/product-detail/zh/avx-corporation/C005YJR05PBSTR/478-7092-2-ND/3444124')
+htmlcode = page.read()
+htmlcodeSTR = htmlcode.decode('utf-8')
+
+pageFile = open('pagecode.txt', 'w')
+pageFile.write(htmlcodeSTR)
+pageFile.close()
+
+
