e1c114cfa8c28b09ddd8f2ff0248e800c4db37ba
Yuanzhe <h568018728@gmail.com>
2018-10-18 22:13:29 +0800

    @Home

--- 001_test.py
+++ 001_test.py
@@ -1,13 +1,37 @@
 #coding:utf-8
 
 from urllib import request
-
-page = request.urlopen('https://www.digikey.com.cn/product-detail/zh/avx-corporation/C005YJR05PBSTR/478-7092-2-ND/3444124')
-htmlcode = page.read()
-htmlcodeSTR = htmlcode.decode('utf-8')
-
-pageFile = open('pagecode.txt', 'w')
-pageFile.write(htmlcodeSTR)
-pageFile.close()
+import re
 
 
+def get_html(url):
+    page = request.urlopen(url)
+    html = page.read()
+    htmlSTR = html.decode('utf-8')
+    return htmlSTR
+
+
+def get_img(html):
+    reg = r'src="(.+?\.jpg)" width'
+    reg_img = re.compile(reg)
+    imglist = reg_img.findall(html)
+    x = 0
+    for img in imglist:
+        print(img)
+        request.urlretrieve(img, '%s.jpg' % x)
+        x = x + 1
+    return
+
+
+print('请输入URL: ')
+url = input()
+if url:
+    pass
+else:
+    url = '__defaulturl__'
+
+get_img(get_html(url))
+
+# pageFile = open('pagecode.txt', 'w', encoding='utf-8')
+# pageFile.write(htmlcode)
+# pageFile.close()
